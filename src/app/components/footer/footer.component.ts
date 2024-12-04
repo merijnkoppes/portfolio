276bf864a3ebc0a86e102fa06990b2983744a82e
MerijnKoppes <koppesmerijn@gmail.com>
2024-12-04 22:50:51 +0100

    image error

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  backgroundimage =
+    'https://firebasestorage.googleapis.com/v0/b/portfolio-e9a65.appspot.com/o/footer%2Flayered-peaks-haikei.png?alt=media&token=ba0cf9ce-1c73-4e87-a0ab-59c872b73d83';
 }
