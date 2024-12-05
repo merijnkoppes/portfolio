58d400df2747ae88e3ec456c99268a0f22625350
MerijnKoppes <koppesmerijn@gmail.com>
2024-12-05 21:05:29 +0100

    end of day

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,7 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
-  backgroundimage =
-    'https://firebasestorage.googleapis.com/v0/b/portfolio-e9a65.appspot.com/o/footer%2Flayered-peaks-haikei.png?alt=media&token=ba0cf9ce-1c73-4e87-a0ab-59c872b73d83';
-}
+export class FooterComponent {}
